4a5dfc1d200d4b5a6296b3fc0a9dd15d25070607
Saddamshah <shahsaddam103@gmail.com>
2019-08-29 05:00:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+
+//Passport Config
+require('./config/passport')(passport);
+
+//config
+const db = require('./config/keys').mongoURL;
+
+//Database 
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('Database Connected...'))
+    .catch(err => console.log(err));
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Body Parser
+app.use(express.urlencoded({ extended: false }));
+
+//Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Coneect Flash
+app.use(flash());
+
+//Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+//Routes
+app.use('/', require('./Routes/index'));    
+app.use('/user', require('./Routes/users'));
+
+//PORT
+const PORT = process.env.port || 5000 
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURL : "mongodb+srv://admin:admin@cluster0-qpuch.mongodb.net/test?retryWrites=true&w=majority"
+}
